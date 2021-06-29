fc342bc04341101d48359c53a17923b328585db8
ghvadym <kravchenkobiz22@gmail.com>
2021-06-29 23:28:36 +0300

    clean code after review

--- wp-content/themes/task-form/header.php
+++ wp-content/themes/task-form/header.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo the_title() ?></title>
-    <link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet">
+<!--    <link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet">-->
     <?php wp_head(); ?>
 </head>
 <body>
